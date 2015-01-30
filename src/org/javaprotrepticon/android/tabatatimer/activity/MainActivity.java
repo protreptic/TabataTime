de6a435514be2091f880988f29a2fc0ceb4a185e
petr_bu <petr_bu@PC00151.volga.magnat.lan>
2015-01-30 15:57:13 +0400

    0

--- src/org/javaprotrepticon/android/tabatatimer/activity/MainActivity.java
+++ src/org/javaprotrepticon/android/tabatatimer/activity/MainActivity.java
@@ -1,7 +1,7 @@
 package org.javaprotrepticon.android.tabatatimer.activity;
 
-import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 	
 }
